2912fbd0b4456653d1ba4caed6f7bef475aca7c7
Denis Ryapolov <denisryapolov@Noutbuk-Denis.local>
2024-03-23 16:30:13 +0300

    test 11

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import readlineSync from 'readline-sync';
+import Logic from '../index.js';
+import { getRandomNumber } from '../randomnumber.js';
 
-const Logic = (gameFunction, rules) => {
-  const counterCorrectAnswer = 3;
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(rules);
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  for (let i = 1; i <= counterCorrectAnswer; i += 1) {
-    const [question, correctAnswer] = gameFunction();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('You answer: ');
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
+const isEven = (num) => num % 2 === 0;
+const getQuestionAndAnswer = () => {
+  const question = getRandomNumber(0, 100);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
 export default Logic;
+
